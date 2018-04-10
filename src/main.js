e946fb21a9dbf2579c7d0530caf234d5f872c630
Edgar Xolop <edgarxoix@gmail.com>
2018-04-09 23:06:27 -0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Bulma from 'bulma/bulma.sass'
+import FontAwesomeWebpack from 'font-awesome-webpack'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
