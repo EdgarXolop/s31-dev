b25737d895e842012012784f86e80e0cc201c930
Edgar Xolop <edgar.xolop@myappsoftware.com>
2018-04-16 14:08:37 -0600

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = (env) => {
   return {
 
     entry: {
-      "platzi-video": path.resolve(__dirname, 'index.js'),
+      "index": path.resolve(__dirname, 'index.js'),
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
